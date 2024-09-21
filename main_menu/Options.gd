2e76d524161a4b5c20fbe77553bb89c85cab673b
Torchyy <102316343+Torchyy@users.noreply.github.com>
2024-09-20 21:29:00 -0600

    Menu now shows current version and credits

--- main_menu/Options.gd
+++ main_menu/Options.gd
@@ -3,4 +3,4 @@ extends TextureButton
 
 func _on_pressed():
 	print("Options")
-	get_tree().change_scene_to_file("res://assets/options_menu/options_menu.tscn")
+	#get_tree().change_scene_to_file("res://assets/options_menu/options_menu.tscn")
