d91f3f19bd230cb1642bda690dfc4a181326d9f4
ethan7palmer <ethan7palmer@gmail.com>
2024-09-22 10:31:32 -0600

    shop opens and you can  click buttons

--- grid_system.gd
+++ grid_system.gd
@@ -1,5 +1,7 @@
 extends TileMapLayer
 
+var shop_instance : Node = null
+
 func _input(event):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 
@@ -11,26 +13,31 @@ func _input(event):
 
 		highlight_tile(clicked_tile)
 
-		var shop_instance = preload("res://menus-and-interfaces/tower_shop/tower_shop.tscn").instantiate()
+		if shop_instance:
+			# if shop is already open close it
+			shop_instance.queue_free()
+			shop_instance = null
+		else:
+			shop_instance = preload("res://menus-and-interfaces/tower_shop/tower_shop.tscn").instantiate()
 
-		# add shop_hud as child of root
-		get_tree().current_scene.add_child(shop_instance)
+			# add shop_hud as child of root
+			get_tree().current_scene.add_child(shop_instance)
 
-		# get width and height of hud
-		var hud_color_rect = shop_instance.get_node("ScrollContainer").get_node("ColorRect")
-		var hud_width = hud_color_rect.get_rect().size.x
-		var hud_height = hud_color_rect.get_rect().size.y
+			# get width and height of hud
+			var hud_color_rect = shop_instance.get_node("ScrollContainer").get_node("ColorRect")
+			var hud_width = hud_color_rect.get_rect().size.x
+			var hud_height = hud_color_rect.get_rect().size.y
 
-		# get size of window
-		var viewport_size = get_viewport().get_visible_rect().size
+			# get size of window
+			var viewport_size = get_viewport().get_visible_rect().size
 
-		var right_margin = 40
-		var top_margin = 50
-		var hud_position_x = (viewport_size.x / 2) - hud_width - right_margin
-		var hud_position_y = -1 * (viewport_size.y / 2) + top_margin
+			var right_margin = 40
+			var top_margin = 50
+			var hud_position_x = (viewport_size.x / 2) - hud_width - right_margin
+			var hud_position_y = -1 * (viewport_size.y / 2) + top_margin
 
-		# mvoe shop to correct position
-		shop_instance.position = Vector2(hud_position_x, hud_position_y)
+			# mvoe shop to correct position
+			shop_instance.position = Vector2(hud_position_x, hud_position_y)
 
 func highlight_tile(tile_pos) -> void:
 	pass
