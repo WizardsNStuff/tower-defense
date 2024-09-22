6ee505349707522d8c40ca11b6d4485abbf2c100
ethan7palmer <ethan7palmer@gmail.com>
2024-09-22 14:16:14 -0600

    wave spawner

--- enemy_spawner.gd
+++ enemy_spawner.gd
@@ -4,14 +4,33 @@ extends Node2D
 @export var spawn_interval = 1.0
 @export var path_follow_node : PathFollow2D
 @onready var spawn_timer : Timer = $SpawnTimer
+@onready var wave_timer : Timer = $WaveTimer
 @export var path_follow_scene = preload("res://path_follow_2d.tscn")
 
+var slime = load("res://enemies/slime.tscn")
+var goblin = load("res://enemies/goblin.tscn")
+var skeleton = load("res://enemies/skeleton.tscn")
+var orc = load("res://enemies/orc.tscn")
+var dragon = load("res://enemies/dragon.tscn")
+	
+var waves = [
+		[slime],
+		[slime,goblin],
+		[slime,goblin,skeleton],
+		[slime,goblin,skeleton,orc],
+		[slime,goblin,skeleton,orc,dragon]
+]
+
+var wave_number = 0
+
 func spawn_enemy() -> void:
+	var wave_mob_size = waves[wave_number].size()
+	
 	# get random index for enemy
-	var random_index = randi_range(0,enemy_scenes.size()-1)
+	var random_index = randi_range(0,wave_mob_size - 1)
 
 	# get random enemy from scene array
-	var enemy_scene = enemy_scenes[random_index]
+	var enemy_scene = waves[wave_number][random_index]
 
 	# create insatnce of enemy scene
 	var enemy_instance = enemy_scene.instantiate()
@@ -27,6 +46,9 @@ func spawn_enemy() -> void:
 	#enemy_instance.position = path_follow_node.position
 	
 	spawn_timer.start()
+	if wave_timer.is_stopped():
+		wave_timer.start()
+	
 
 func _on_spawn_timer_timeout() -> void:
 	pass # spawn timer finished, spawn again
@@ -34,3 +56,19 @@ func _on_spawn_timer_timeout() -> void:
 func _physics_process(_delta: float) -> void:
 	if spawn_timer.is_stopped():
 		spawn_enemy()
+	if wave_timer.is_stopped():
+		# goto next wave
+		# win
+		print("wave number: ", wave_number + 1)
+		if wave_number >= enemy_scenes.size()-1:
+			#load win scene
+			# if no enemies on screen
+			print("WINNNN")
+			pass
+		else:
+			wave_number += 1
+			spawn_enemy()
+
+
+func _on_wave_timer_timeout() -> void:
+	pass # Replace with function body.
