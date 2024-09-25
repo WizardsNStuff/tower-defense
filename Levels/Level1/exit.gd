be85cf0c678c28188d836bd7bf6f00c6f9b0629b
Torchyy <102316343+Torchyy@users.noreply.github.com>
2024-09-24 21:38:26 -0600

    Changed HP to text, added collision for path but needs to be implemented

--- Levels/Level1/exit.gd
+++ Levels/Level1/exit.gd
@@ -1,12 +1,10 @@
 extends Area2D
 
 @onready var totalHealth : int = 100
-@onready var Player_health_bar : ProgressBar = $HealthBar
  
 func _on_body_entered(body: Node2D) -> void:
 	if body is Enemy:
 		totalHealth -= body.damage
-		Player_health_bar.value -= body.damage
 		if totalHealth <= 0:
 			get_tree().change_scene_to_file("res://menus-and-interfaces/main_menu/Main_Menu.tscn")
 			GameOver.showGameOver = true
